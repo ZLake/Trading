73a0120cc873a10588ef44655df5462c1c105b86
lake.zl <lake.zl@alibaba-inc.com>
2017-08-11 10:23:04 +0800

    list read

--- m3/code/read_data.py
+++ m3/code/read_data.py
@@ -54,17 +54,26 @@ def read(split_num=1200,dest='../input/'):
     test = pd.DataFrame()
     imp_print2("Start Reading Data")
     imp_print2("Train set",3)
+    
+    train = pd.DataFrame()
+    train_list = []
     for i in range(files_list.index(split_num)+1):
         temp_train = pd.read_csv(dest+str(files_list[i])+".csv",header=None)
         temp_train.insert(0, 'csv_index', files_list[i])
-        train = pd.concat((train,temp_train)).reset_index(drop=True)
-        print('Training set now processing: '+ str(files_list[i])+".csv, after train sample number: "+ str(len(train)))
+        train_list.append(temp_train)
+        print('Training set now processing: '+ str(files_list[i])+".csv, sample number: "+ str(len(temp_train)))
+    train = pd.concat(train_list)
+    
     imp_print2("Test set",3)
+    test = pd.DataFrame()
+    test_list = []
     for j in range(files_list.index(split_num)+1,len(files_list)):
         temp_test = pd.read_csv(dest+str(files_list[j])+".csv",header=None)
         temp_test.insert(0, 'csv_index', files_list[j])
-        test = pd.concat((test,temp_test)).reset_index(drop=True)
-        print('Testing  set now processing: '+ str(files_list[j])+".csv, after test sample number: "+ str(len(test)))
+        test_list.append(temp_test)
+        print('Testing  set now processing: '+ str(files_list[j])+".csv, sample number: "+ str(len(temp_test)))
+    test = pd.concat(test_list)
+
     imp_print2("End Reading Data",11)
     print ("train set size:{}".format(train.shape))
     print ("test set size:{}".format(test.shape))
