bf38ab01fefeb645ff4d00bea6c60d3c6cee1d19
lake.zl <lake.zl@alibaba-inc.com>
2017-08-16 16:39:09 +0800

    update

--- m3/code/single_model.py
+++ m3/code/single_model.py
@@ -111,164 +111,167 @@ def evaluate_test(model,params,topks=[50,30,10]):
                                         ,temp_min,temp_max,temp_simple_avg]
     return eval_df
 
-#####################
-## define global parameters
-Params = {}
-Params['algo'] = ['model_lgb'] # 可选参数：lasso,model_lgb
-# lasso params
-Params['lasso_grid_params'] = dict(scaler=[StandardScaler()]
-                              ,lasso__alpha=[0.0001,0.0005,0.001,0.002,0.005,0.01,0.05])
-# lgb params
-Params['model_lgb_grid_params'] = {
-    'learning_rate': [0.02]
-    ,'n_estimators': [48]
-    ,'num_leaves': [18]
-    ,'boosting_type' : ['gbdt']
-    ,'objective' : ['regression']
-    ,'seed' : [500]
-    ,'colsample_bytree' : [0.6]
-    ,'subsample' : [0.75]
-#    ,'reg_alpha' : [1,2,6]
-#    ,'reg_lambda' : [1,2,6]
-    }
-Params['topK'] = 50 # 选股个数
-Params['topK_params'] = 1 # 前k个参数用于实际测试 
-
-result_list = []
-#####################
-# Read the data: 选择数据的时间段
-#####################
-imp_print("Data Loading...",40)
-read_start = time.time()
-# 数据格式 hdf5
-train_raw = pd.read_hdf('DataSet/train_1200_1333.h5')
-test_raw = pd.read_hdf('DataSet/test_1200_1333.h5')
-# 选择数据时间段：todo
-train = train_raw
-test=test_raw
-#check the numbers of samples and features
-print("The train data size before dropping Id feature is : {} ".format(train.shape))
-print("The test data size before dropping Id feature is : {} ".format(test.shape))
-#Save the 'csv_index' column
-train_csv_index = train[train.columns[0]]
-test_csv_index = test[train.columns[0]]
-#Save the 'Id' column
-train_ID = train[train.columns[1]]
-test_ID = test[train.columns[1]]
-#Now drop the  'csv_index' & 'Id' colum since it's unnecessary for  the prediction process.
-train.drop(train.columns[0:2], axis = 1, inplace = True)
-test.drop(test.columns[0:2], axis = 1, inplace = True)
-#check again the data size after dropping the 'Id' variable
-read_end = time.time()
-print("\nThe train data size after dropping Id feature is : {} ".format(train.shape)) 
-print("The test data size after dropping Id feature is : {} ".format(test.shape))
-# 
-#####################
-# Preprocess: 处理成训练和测试集合
-#####################
-imp_print("Data Processing...",40)
-proc_start = time.time()
-#############
-ntrain = train.shape[0]
-ntest = test.shape[0]
-y_train = train[train.columns[0]].values
-y_test = test[test.columns[0]].values
-all_data = pd.concat((train, test)).reset_index(drop=True)
-y_all_data = all_data[all_data.columns[0]].values
-all_data.drop(train.columns[0], axis=1, inplace=True)
-print("all_data size is : {}".format(all_data.shape))
-# missing data
-all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
-all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
-missing_data = pd.DataFrame({'Missing Ratio' :all_data_na})
-missing_data.head(20)
-print("missing data column numbers:{}".format(missing_data.shape[0]))
-if(missing_data.shape[0] == 0):
-    imp_print("no missing data, go to next step...")
-else:
-    imp_print("Need filling missing data...")
-proc_end = time.time()
-#####################
-# Modeling: 建模
-#####################
-imp_print("Modeling...",40)
-model_start = time.time()
-# get the train and val and test data
-train = all_data[:ntrain]
-test = all_data[ntrain:]
-######
-# Lasso Regression
-######
-scaler = StandardScaler()
-#scaler = RobustScaler()
-lasso = Pipeline(steps=[('scaler',scaler),
-                      ('lasso',Lasso(alpha = 0.01,random_state=1))])
-######
-# LightGBM
-######
-model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
-                              learning_rate=0.05, n_estimators=720,
-                              max_bin = 55, bagging_fraction = 0.8,
-                              bagging_freq = 5, feature_fraction = 0.2319,
-                              feature_fraction_seed=9, bagging_seed=9,
-                              min_data_in_leaf =6, min_sum_hessian_in_leaf = 11
-                              )
-#grid search params
-for algo in Params['algo']:
-    imp_print(algo,20)
-    param_grid = Params[algo + '_grid_params']
-    estimator = eval(algo)
-    # grid_search
-    # each csv is one test set
-    custom_cv = PredefinedSplit([-1]*ntrain+[1]*ntest)
-    customized_cv = [(range(ntrain),range(ntrain+ntest))]
-    # get csv_index in the test set for gridsearch evaluation
-    test_csvIndexs = test_csv_index.unique()
-    
-    reggressor = GridSearchCV(estimator
-                               , param_grid=param_grid
-    #                           ,scoring = 'neg_mean_squared_error'
-                               ,scoring = top50_avg_loss
-                               ,cv = custom_cv
-                               ,refit = False
-                               ,n_jobs=1
-                               ,verbose=1
-                               ,return_train_score=False
-                               ,pre_dispatch = 1)
-    
-    joblib.dump(all_data.values, 'DataSet/temp_all_data')
-    all_data_values = joblib.load('DataSet/temp_all_data', mmap_mode='r+')
-    reggressor.fit(all_data_values,y_all_data)
-    
-    print('grid search result:')
-    print('best score:'+ str(reggressor.best_score_))
-    print('best params:'+ str(reggressor.best_params_))
-    print('detailed results:',report(reggressor.cv_results_))
-    rank_params = pd.DataFrame(columns=['Rank'
-                                        ,'CV_score'
-                                        ,'params'])
-    for rank in range(1,Params['topK_params']+1):
-        report_params(reggressor.cv_results_,rank,rank_params)
-          
+def training():
     #####################
-    # # Test: 测试获取评价结果
-    #####################
-    imp_print("Testing...",40)
-    for rank in range(Params['topK_params']):
-        temp_params = rank_params['params'][rank]
-        print('CV rank {} model:\n\
-              params:{}'.format(rank+1,temp_params))
-        eval_df = evaluate_test(model=estimator,params=temp_params)
+    ## define global parameters
+    Params = {}
+    Params['algo'] = ['model_lgb'] # 可选参数：lasso,model_lgb
+    # lasso params
+    Params['lasso_grid_params'] = dict(scaler=[StandardScaler()]
+                                  ,lasso__alpha=[0.0001,0.0005,0.001,0.002,0.005,0.01,0.05])
+    # lgb params
+    Params['model_lgb_grid_params'] = {
+        'learning_rate': [0.02]
+        ,'n_estimators': [48]
+        ,'num_leaves': [18]
+        ,'boosting_type' : ['gbdt']
+        ,'objective' : ['regression']
+        ,'seed' : [500]
+        ,'colsample_bytree' : [0.6]
+        ,'subsample' : [0.75]
+    #    ,'reg_alpha' : [1,2,6]
+    #    ,'reg_lambda' : [1,2,6]
+        }
+    Params['topK'] = 50 # 选股个数
+    Params['topK_params'] = 1 # 前k个参数用于实际测试 
     
-        print('simple_avg:{}'.format(eval_df['simple_avg'].mean()))
+    result_list = []
+    #####################
+    # Read the data: 选择数据的时间段
+    #####################
+    imp_print("Data Loading...",40)
+    read_start = time.time()
+    # 数据格式 hdf5
+    train_raw = pd.read_hdf('DataSet/train_1331_1333.h5')
+    test_raw = pd.read_hdf('DataSet/test_1331_1333.h5')
+    # 选择数据时间段：todo
+    train = train_raw
+    test=test_raw
+    #check the numbers of samples and features
+    print("The train data size before dropping Id feature is : {} ".format(train.shape))
+    print("The test data size before dropping Id feature is : {} ".format(test.shape))
+    #Save the 'csv_index' column
+    train_csv_index = train[train.columns[0]]
+    test_csv_index = test[train.columns[0]]
+    #Save the 'Id' column
+    train_ID = train[train.columns[1]]
+    test_ID = test[train.columns[1]]
+    #Now drop the  'csv_index' & 'Id' colum since it's unnecessary for  the prediction process.
+    train.drop(train.columns[0:2], axis = 1, inplace = True)
+    test.drop(test.columns[0:2], axis = 1, inplace = True)
+    #check again the data size after dropping the 'Id' variable
+    read_end = time.time()
+    print("\nThe train data size after dropping Id feature is : {} ".format(train.shape)) 
+    print("The test data size after dropping Id feature is : {} ".format(test.shape))
+    # 
+    #####################
+    # Preprocess: 处理成训练和测试集合
+    #####################
+    imp_print("Data Processing...",40)
+    proc_start = time.time()
+    #############
+    ntrain = train.shape[0]
+    ntest = test.shape[0]
+    y_train = train[train.columns[0]].values
+    y_test = test[test.columns[0]].values
+    all_data = pd.concat((train, test)).reset_index(drop=True)
+    y_all_data = all_data[all_data.columns[0]].values
+    all_data.drop(train.columns[0], axis=1, inplace=True)
+    print("all_data size is : {}".format(all_data.shape))
+    # missing data
+    all_data_na = (all_data.isnull().sum() / len(all_data)) * 100
+    all_data_na = all_data_na.drop(all_data_na[all_data_na == 0].index).sort_values(ascending=False)[:30]
+    missing_data = pd.DataFrame({'Missing Ratio' :all_data_na})
+    missing_data.head(20)
+    print("missing data column numbers:{}".format(missing_data.shape[0]))
+    if(missing_data.shape[0] == 0):
+        imp_print("no missing data, go to next step...")
+    else:
+        imp_print("Need filling missing data...")
+    proc_end = time.time()
+    #####################
+    # Modeling: 建模
+    #####################
+    imp_print("Modeling...",40)
+    model_start = time.time()
+    # get the train and val and test data
+    train = all_data[:ntrain]
+    test = all_data[ntrain:]
+    ######
+    # Lasso Regression
+    ######
+    scaler = StandardScaler()
+    #scaler = RobustScaler()
+    lasso = Pipeline(steps=[('scaler',scaler),
+                          ('lasso',Lasso(alpha = 0.01,random_state=1))])
+    ######
+    # LightGBM
+    ######
+    model_lgb = lgb.LGBMRegressor(objective='regression',num_leaves=5,
+                                  learning_rate=0.05, n_estimators=720,
+                                  max_bin = 55, bagging_fraction = 0.8,
+                                  bagging_freq = 5, feature_fraction = 0.2319,
+                                  feature_fraction_seed=9, bagging_seed=9,
+                                  min_data_in_leaf =6, min_sum_hessian_in_leaf = 11
+                                  )
+    #grid search params
+    for algo in Params['algo']:
+        imp_print(algo,20)
+        param_grid = Params[algo + '_grid_params']
+        estimator = eval(algo)
+        # grid_search
+        # each csv is one test set
+        custom_cv = PredefinedSplit([-1]*ntrain+[1]*ntest)
+        customized_cv = [(range(ntrain),range(ntrain+ntest))]
+        # get csv_index in the test set for gridsearch evaluation
+        test_csvIndexs = test_csv_index.unique()
         
-        for topk in eval_df['topk'].unique():
-            print('top'+str(int(topk))+' avg:{}'.format(str(eval_df['pred_avg'][eval_df['topk']==topk].mean())))
-model_end = time.time()
-   
-imp_print('Execution Time:')
-print("Reading data time cost: {}s".format(read_end - read_start))
-print("Processing data time cost: {}s".format(proc_end - proc_start))
-print("Modelling & Test data time cost: {}s".format(model_end - model_start))
+        reggressor = GridSearchCV(estimator
+                                   , param_grid=param_grid
+        #                           ,scoring = 'neg_mean_squared_error'
+                                   ,scoring = top50_avg_loss
+                                   ,cv = custom_cv
+                                   ,refit = False
+                                   ,n_jobs=1
+                                   ,verbose=1
+                                   ,return_train_score=False
+                                   ,pre_dispatch = 1)
+        
+        joblib.dump(all_data.values, 'DataSet/temp_all_data')
+        all_data_values = joblib.load('DataSet/temp_all_data', mmap_mode='r+')
+        reggressor.fit(all_data_values,y_all_data)
+        
+        print('grid search result:')
+        print('best score:'+ str(reggressor.best_score_))
+        print('best params:'+ str(reggressor.best_params_))
+        print('detailed results:',report(reggressor.cv_results_))
+        rank_params = pd.DataFrame(columns=['Rank'
+                                            ,'CV_score'
+                                            ,'params'])
+        for rank in range(1,Params['topK_params']+1):
+            report_params(reggressor.cv_results_,rank,rank_params)
+              
+        #####################
+        # # Test: 测试获取评价结果
+        #####################
+        imp_print("Testing...",40)
+        for rank in range(Params['topK_params']):
+            temp_params = rank_params['params'][rank]
+            print('CV rank {} model:\n\
+                  params:{}'.format(rank+1,temp_params))
+            eval_df = evaluate_test(model=estimator,params=temp_params)
+        
+            print('simple_avg:{}'.format(eval_df['simple_avg'].mean()))
+            
+            for topk in eval_df['topk'].unique():
+                print('top'+str(int(topk))+' avg:{}'.format(str(eval_df['pred_avg'][eval_df['topk']==topk].mean())))
+    model_end = time.time()
+       
+    imp_print('Execution Time:')
+    print("Reading data time cost: {}s".format(read_end - read_start))
+    print("Processing data time cost: {}s".format(proc_end - proc_start))
+    print("Modelling & Test data time cost: {}s".format(model_end - model_start))
 
-print ("Finished...")
+if __name__ == "__main__":
+    training()
+    print ("Finished...")
