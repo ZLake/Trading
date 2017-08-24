0795b6442030edda9e5cfc7c69f76fb1dd9ab47a
lake.zl <lake.zl@alibaba-inc.com>
2017-08-24 15:01:52 +0800

    update

---
+++ m3/code/check_results.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 24 14:55:19 2017
+
+@author: lakezhang
+"""
+import pandas as pd
+
+def get_result(theme,train_name_raw,test_name_raw):
+    file_name = train_name_raw.strip('.h5').strip('train_')
+    full_path = 'Result/'+file_name+'/'+theme+'__' + file_name+'_result.h5'
+    result_df = pd.read_hdf(full_path,engine = 'c')
+    return result_df
+    
+    print('reading success')
+if __name__ == "__main__":
+    theme = 'ODTest'
+    train_name_raw = 'train_1200_1333.h5'
+    test_name_raw = 'test_1200_1333.h5'
+    result_df = get_result(theme,train_name_raw,test_name_raw)
+    
+    print ("Finished...")
--- m3/code/single_model_2.py
+++ m3/code/single_model_2.py
@@ -33,61 +33,10 @@ import gc
 import os
 import warnings
 
-
+from params import get_params
 from simple_functions import imp_print
 from outlier_detection import outlier_detection
 from evaluation import evaluate_test,store_result
-
-def get_params():
-    #####################
-    ## define global parameters
-    Params = {}
-    # data files
-    if(multiprocessing.cpu_count() >=60):
-        Params['train_name_raw'] = 'train_1200_1333.h5'
-        Params['test_name_raw'] = 'test_1200_1333.h5'
-    else:
-        Params['train_name_raw'] ='train_1331_1333.h5'
-        Params['test_name_raw'] = 'test_1331_1333.h5'
-    # theme 
-    Params['theme'] = 'ODTest'# 本次运行的目的
-    ########## Outlier detection params
-    IF_Params = {'max_samples':0.7
-                 ,'n_estimators':100
-                 ,'contamination':0.1} # 0.1
-    LOF_Params = {'n_neighbors':20
-                  ,'algorithm':'ball_tree'
-                  ,'leaf_size':30
-                  ,'metric':'minkowski'
-                  ,'p':2
-                  ,'contamination':0.1
-                    }
-    Params['Outlier_Detector'] = {'algo':'IF'                 # None,IF:IsolationForest,LOF
-                                  ,'apply_on_test':True
-                                  ,'IF_Params':IF_Params
-                                  ,'LOF_Params':LOF_Params}
-    ########## Modeling parmas
-    Params['algo'] = ['lasso'] # 可选参数： lasso,model_lgb
-    ######## 注意： 因为内存不够问题，lasso的StandardScaler(copy=False)，会对train 做inplace 替换！！！
-    # lasso params
-    Params['lasso_grid_params'] = dict(scaler=[StandardScaler()]
-                                  ,lasso__alpha=[0.0001,0.0005,0.001,0.002,0.005,0.01,0.05])
-    # lgb params
-    Params['model_lgb_grid_params'] = {
-        'learning_rate': [0.02]
-        ,'n_estimators': [48]
-        ,'num_leaves': [18]
-        ,'boosting_type' : ['gbdt']
-        ,'objective' : ['regression']
-        ,'seed' : [500]
-        ,'colsample_bytree' : [0.6]
-        ,'subsample' : [0.75]
-    #    ,'reg_alpha' : [1,2,6]
-    #    ,'reg_lambda' : [1,2,6]
-        }
-    ########## Evaluation params
-    Params['topK'] = 50 # 选股个数
-    return Params
 
 def training():
     Params = get_params()
