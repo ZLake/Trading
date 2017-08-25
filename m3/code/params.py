c5a2aa121bfc3dde519ac4cf32165cfe395eaa8e
lake.zl <lake.zl@alibaba-inc.com>
2017-08-25 17:55:17 +0800

    update

--- m3/code/params.py
+++ m3/code/params.py
@@ -8,6 +8,8 @@ Created on Thu Aug 24 14:51:13 2017
 import multiprocessing
 from sklearn.preprocessing import RobustScaler,StandardScaler
 from sklearn.model_selection import ParameterGrid
+import pandas as pd
+import os
 
 def get_params():
     #####################
@@ -26,6 +28,7 @@ def get_params():
     IF_Params = {'max_samples':0.7
                  ,'n_estimators':100
                  ,'contamination':0.1} # 0.1
+    IF_Grid_Params = {'max_samples':[0.7,0.8]}
     LOF_Params = {'n_neighbors':20
                   ,'algorithm':'ball_tree'
                   ,'leaf_size':30
@@ -33,17 +36,22 @@ def get_params():
                   ,'p':2
                   ,'contamination':0.1
                     }
+    LOF_Grid_Params = {}
     Params['Outlier_Detector'] = {'algo':'None'                 # None,IF:IsolationForest,LOF
                                   ,'apply_on_test':True
                                   ,'IF_Params':IF_Params
-                                  ,'LOF_Params':LOF_Params}
+                                  ,'IF_Grid_Params':IF_Grid_Params
+                                  ,'LOF_Params':LOF_Params
+                                  ,'LOF_Grid_Params':LOF_Grid_Params}
     ########## Modeling parmas
-    Params['algo'] = ['model_lgb'] # 可选参数： lasso,model_lgb
+    Params['algo'] = ['lasso'] # 可选参数： lasso,model_lgb
     ######## 注意： 因为内存不够问题，lasso的StandardScaler(copy=False)，会对train 做inplace 替换！！！
     # lasso params
+    Params['lasso_default_params'] = {}
     Params['lasso_grid_params'] = dict(scaler=[StandardScaler()]
-                                  ,lasso__alpha=[0.0001,0.0005,0.001,0.002,0.005,0.01,0.05])
+                                  ,lasso__alpha=[0.001,0.002,0.005])
     # lgb params
+    Params['model_lgb_default_params'] = {} 
     Params['model_lgb_grid_params'] = {
         'learning_rate': [0.02]
         ,'n_estimators': [48]
@@ -64,10 +72,31 @@ def paramGridSearch(params):
     params_combs = ParameterGrid(params)
     return params_combs
 
-
+def load_params_combs(theme,train_name_raw,params,continue_mode = True):
+    file_name = train_name_raw.strip('.h5').strip('train_')
+    full_path = 'Result/'+file_name+'/'+theme+'__' + 'param_combs.h5'
+    if not os.path.exists('Result/'+file_name):
+        os.makedirs('Result/'+file_name)
+    if (os.path.exists(full_path) and continue_mode):
+        print('Get parameter combinations from: ' + full_path)
+        param_combs_df = pd.read_hdf(full_path,engine = 'c')
+    else:
+        print('Generating parameter combinations...')
+        param_combs_df = pd.DataFrame(columns=['NO.','params','status'])
+        param_combs = paramGridSearch(params)
+        ind = 1
+        for param in param_combs:
+            param_combs_df.loc[len(param_combs_df)] = [ind,param,0]
+            ind+=1
+    return param_combs_df
+        
 if __name__ == "__main__":
-    params = {'max_samples':[0.7,0.8]
+    params_od = {'max_samples':[0.7,0.8]
                  ,'n_estimators':[100]
                  ,'contamination':[0.1]} # 0.1
-    paramGridSearch()
+    params_lasso = dict(scaler=[StandardScaler()]
+                                  ,lasso__alpha=[0.0001,0.0005,0.001,0.002,0.005,0.01,0.05])
+    params ={}
+    params_combs = load_params_combs('test','train_1331_1333.h5',params)
+    
     print ("Finished...")
