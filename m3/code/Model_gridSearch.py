f033ea0e6cc5996b49921f5556c30c3b051b098f
lake.zl <lake.zl@alibaba-inc.com>
2017-08-29 17:22:24 +0800

    update

--- m3/code/Model_gridSearch.py
+++ m3/code/Model_gridSearch.py
@@ -205,7 +205,7 @@ def training():
                 cost_time = (temp_time_end-temp_time_start)/60                # min
                 store_result(Params,algo_grid_param,algo,eval_df,estimator
                              ,train_name_raw,test_name_raw,Params['theme'],cost_time)
-
+                print('Cost time:{}'.format(cost_time))
                 print('garbage collection:{}'.format(gc.collect()))
                 #update done info for grid search:algo
                 update_params_combs(Params['theme'],train_name_raw
